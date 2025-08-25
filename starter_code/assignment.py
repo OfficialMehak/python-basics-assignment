3567a2115827351832846fb6c939bddc4d23a058
Mehakpreet Singh <159048970+OfficialMehak@users.noreply.github.com>
2025-08-25 15:51:50 +0530

    Create assignment.py

---
+++ starter_code/assignment.py
@@ -0,0 +1,26 @@
+# Assignment Starter Code
+# Complete the functions below. Do NOT change the function names or signatures.
+
+def add_numbers(a: int, b: int) -> int:
+    """
+    Return the sum of two integers.
+    """
+    # TODO: Implement this
+    pass
+
+
+def is_palindrome(s: str) -> bool:
+    """
+    Return True if the string is a palindrome, False otherwise.
+    Ignore case and spaces.
+    """
+    # TODO: Implement this
+    pass
+
+
+def find_max(lst: list[int]) -> int:
+    """
+    Return the maximum number in a list.
+    """
+    # TODO: Implement this
+    pass
